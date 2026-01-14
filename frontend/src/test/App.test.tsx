8cfa252ed4be0224333019ccccd913d7591c056c
Jasen Alfatama <jasenalfatama9@gmail.com>
2026-01-15 03:02:46 +0700

    fixing kegagalan deployment ke vercel

--- frontend/src/test/App.test.tsx
+++ frontend/src/test/App.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen, fireEvent, waitFor } from '@testing-library/react'
+import { render, screen, fireEvent } from '@testing-library/react'
 import { expect, test, vi, beforeEach } from 'vitest'
 import App from '../App'
 
